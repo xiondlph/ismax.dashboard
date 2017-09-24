de48f1ed8bf0737c7c357366d6ff13a80a75479a
Ismailov Shuhrat <admin@ismax.ru>
2017-09-24 23:56:05 +0300

    Пустой, базовый вариант React приложения

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,53 @@
-var path = require('path'),
-    HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-    entry: './src/index.jsx',
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        './src/index'
+    ],
     output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'build.js'
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
     },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
     module: {
-        rules: [
-            { test: /\.(jsx)$/, use: 'babel-loader' }
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['eslint-loader'],
+                include: [
+                    path.resolve(__dirname, 'src'),
+                ],
+                enforce: 'pre'
+            },
+            {
+                loaders: 'react-hot-loader',
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+                test: /\.js$/,
+                options: {
+                    plugins: ['transform-runtime']
+                },
+            },
+            {
+                loaders: 'babel-loader',
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+                test: /\.js$/,
+                options: {
+                    plugins: ['transform-runtime']
+                },
+            }
         ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: 'src/index.html'
-    })]
+    }
 }

83d3e9a0b34480952c8c310fa747ab89d0229a9a
Ismailov Shuhrat <admin@ismax.ru>
2017-09-29 00:28:16 +0300

    Точно норм вариант

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,61 +2,6 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
     render() {
-        return <div className='cont'>
-            <div className='left'>
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-            </div>
-            <div className='right'>
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-                shorarinde shorarinde shorarinde shorarinde shorarinde
-            </div>
-        </div>
+        return <div>Привет из App</div>
     }
 }

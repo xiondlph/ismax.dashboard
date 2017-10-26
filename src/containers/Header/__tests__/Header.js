e23233040aceeb78c233f21565f88fd8378b6e97
sh.ismailov <sh.ismailov@solarsecurity.ru>
2017-10-26 18:10:47 +0300

    fix теста Header

--- src/containers/Header/__tests__/Header.js
+++ src/containers/Header/__tests__/Header.js
@@ -2,14 +2,24 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import Header from '../Header'
+import Header from '../index'
+
+import {
+	DASHBOARD_SIDEBAR_STATE_EXPANDED,
+	DASHBOARD_SIDEBAR_STATE_COLLAPSED
+} from '../../../constants/Dashboard'
 
 configure({ adapter: new Adapter() });
 
 test('Проверка Header снапшота', () => {
-    const component = renderer.create(
-        <Header />
-    )
+    const
+		sidebar = DASHBOARD_SIDEBAR_STATE_COLLAPSED,
+		profile = {},
+		setSidebarState = jest.fn(),
+		setMobileSidebarState = jest.fn(),
+        component = renderer.create(
+            <Header sidebar={sidebar} profile={profile} setSidebarState={setSidebarState} setMobileSidebarState={setMobileSidebarState}/>
+        )
 
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
@@ -17,17 +27,23 @@ test('Проверка Header снапшота', () => {
 
 test('Проверка смены состояния Sidebar', () => {
     const
-        sidebar = 'mini',
-        setSidebarState = () => {
-            header.setProps({ sidebar: 'full' })
+        sidebar = DASHBOARD_SIDEBAR_STATE_COLLAPSED,
+        profile = {
+            email: 'test@email.com'
         },
+        setSidebarState = () => {
+            header.setProps({ sidebar: DASHBOARD_SIDEBAR_STATE_EXPANDED })
+        },
+		setMobileSidebarState = () => {
+			header.setProps({ sidebar: DASHBOARD_SIDEBAR_STATE_EXPANDED })
+		},
         header = shallow(
-            <Header sidebar={sidebar} setSidebarState={setSidebarState}/>
+            <Header sidebar={sidebar} profile={profile} setSidebarState={setSidebarState} setMobileSidebarState={setMobileSidebarState} />
         );
 
     expect(header.find('.dashboard-header-logo').exists()).toEqual(true)
-    expect(header.find('.dashboard-header-logo').hasClass('dashboard-header-logo_mini')).toEqual(true)
+    expect(header.find('.dashboard-header-logo').hasClass('dashboard-header-logo_collapsed')).toEqual(true)
 
     header.find('.dashboard-header-toolbar a').simulate('click');
-    expect(header.find('.dashboard-header-logo').hasClass('dashboard-header-logo_full')).toEqual(true)
+    expect(header.find('.dashboard-header-logo').hasClass('dashboard-header-logo_collapsed')).toEqual(false)
 })
